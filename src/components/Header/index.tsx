2eda05bce55f608c56cdabdbf7535ecf53217f98
Igor José <igorj000@gmail.com>
2022-03-09 22:48:45 -0300

    adjusting header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,31 @@
 
+import Link from 'next/link'
+import { useRouter } from 'next/router';
+import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
+  const router = useRouter();
+
+  function isActivePage(path: string): string {
+    return router.pathname === path ? styles.active : ''
+  }
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={isActivePage("/")}>Home</a>
+          </Link>
+
+          <Link href="posts">
+            <a className={isActivePage("/posts")}>Posts</a>
+          </Link>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
