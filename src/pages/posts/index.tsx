c9598615710eff4ad542dad988a2b1198ed7a921
Igor José <igorj000@gmail.com>
2022-03-09 22:50:41 -0300

    create posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next"
+import Head from "next/head"
+import { Header } from "../../components/Header"
+
+const Posts: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Posts | ig.news</title>
+      </Head>
+
+      <Header />
+
+    </>
+  )
+}
+
+export default Posts
